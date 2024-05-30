5c39ccabfa2e2431d16106aa260be12acf4b6d7d
tibbar213 <1786723492@qq.com>
2024-05-30 08:57:34 +0800

    修复无法启动问题

--- app/manage.py
+++ app/manage.py
@@ -1,14 +1,8 @@
-from flask import Flask
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
 from app import create_app, db
+from flask_migrate import Migrate
 
 app = create_app()
 migrate = Migrate(app, db)
-manager = Manager(app)
-
-# Add the migration commands to the manager
-manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
